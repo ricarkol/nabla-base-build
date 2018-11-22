d73f8c1ed1063dff994f3d89136aa88016288a77
Ricardo Koller <kollerr@us.ibm.com>
2018-11-22 00:46:18 -0600

    run_per.sh with 'set location'

--- postmark-base/run_perf.sh
+++ postmark-base/run_perf.sh
@@ -1,13 +1,17 @@
 name=$1
 workload=$2
+location=$3
 
 mkdir -p data
+
+echo "set location ${location}" > /tmp/pm.location
+cat /tmp/pm.location benchmark.${workload} > /tmp/pm.config
 
 for i in `seq 1 2`
 do
 	ts=$(date +%s%N)
-	time ./postmark.linux benchmark.${workload} >> data/postmark.${workload}.${name}.out
-	tx=`grep transactions benchmark.${workload} | awk '{print $3}'`
+	time ./postmark.linux /tmp/pm.config >> data/postmark.${workload}.${name}.out
+	tx=`grep transactions /tmp/pm.config | awk '{print $3}'`
 	tn=$((($(date +%s%N) - $ts)))
 	tt=`echo "scale=8; $tn/1000000000" | bc -l`
 	tp=`echo "scale=4; $tx/$tt" | bc -l`
