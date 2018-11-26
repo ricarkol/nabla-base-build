5d3e5e97b6c74c06815e38018f2a2809123b1144
Ricardo Koller <kollerr@us.ibm.com>
2018-11-26 15:18:16 -0600

    More on postmark experiments

--- postmark-base/run_perf.sh
+++ postmark-base/run_perf.sh
@@ -11,11 +11,11 @@ mkdir -p data
 echo "set location ${location}" > /tmp/pm.location
 cat /tmp/pm.location benchmark.${workload} > /tmp/pm.config
 
-for i in `seq 1 5`
+for i in `seq 1 3`
 do
 	ts=$(date +%s%N)
 	time ./postmark.linux /tmp/pm.config >> data/postmark.${workload}.${name}.out
-	tx=`grep transactions /tmp/pm.config | awk '{print $3}'`
+	tx=`grep transactions benchmark.${workload} | awk '{print $3}'`
 	tn=$((($(date +%s%N) - $ts)))
 	tt=`echo "scale=8; $tn/1000000000" | bc -l`
 	tp=`echo "scale=4; $tx/$tt" | bc -l`
