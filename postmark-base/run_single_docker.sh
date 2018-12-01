990d2b2dedca88e5db89246785f027b26e802dc4
Ricardo Koller <kollerr@us.ibm.com>
2018-12-01 12:37:17 -0600

    Adding --runtime option to run_single_docker*

--- postmark-base/run_single_docker.sh
+++ postmark-base/run_single_docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Example:
-# bash run_single_docker.sh read5.med
+# bash run_single_docker.sh read5.med runc
 
 workload=$1
+runtime=$2
 
-docker run -it --rm --net=host --cap-add=NET_ADMIN \
+docker run -it --runtime=${runtime} --rm --cap-add=NET_ADMIN \
 	--device /dev/net/tun:/dev/net/tun -w /postmark \
 	-v `pwd`:/postmark kollerr/postmark-experiments \
 	bash -c "bash run_single_linux.sh ${workload} /data 2> /dev/null"
--- postmark-base/run_single_docker_cow.sh
+++ postmark-base/run_single_docker_cow.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Example:
-# bash run_single_docker.sh read5.med
+# bash run_single_docker.sh read5.med runc
 
 workload=$1
+runtime=$2
 
-docker run -it --rm --net=host --cap-add=NET_ADMIN \
+docker run -it --rm --runtime=${runtime} --cap-add=NET_ADMIN \
 	--device /dev/net/tun:/dev/net/tun -w /postmark \
 	-v `pwd`:/postmark kollerr/postmark-base-${workload} \
 	bash -c "bash run_single_linux.sh ${workload} /pm_data keep 2> /dev/null"
