b8c00e8d77c8c2b16a3c0bdc456d4a7e4f2b59ea
Ricardo Koller <kollerr@us.ibm.com>
2018-12-01 13:44:42 -0500

    Adding build_docker_postmark_bases.sh

---
+++ postmark-base/build_docker_postmark_bases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Example:
+# bash build_docker_postmark_bases.sh
+
+location=pm_data
+
+for workload in `ls benchmark.read*.*`
+do
+	mkdir -p pm_data
+	rm -rf pm_data/* # just in case
+
+	echo "set location ${location}" > /tmp/pm.config
+	grep -v "set location" benchmark.${workload} | sed s/run/prepare/g >> /tmp/pm.config
+	./postmark-split.linux /tmp/pm.config
+
+	# Dockerfile.postmark-base looks like:
+	# FROM kollerr/postmark-experiments
+	# RUN mkdir /pm_data
+	# COPY * /pm_data/.
+
+	cp Dockerfile.postmark-base ${location}/.
+	( cd ${location} && sudo docker build -f Dockerfile.postmark-base -t kollerr/postmark-base-${workload} . )
+done
