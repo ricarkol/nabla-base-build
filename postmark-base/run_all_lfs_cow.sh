f2d78df509d4e7121aa4b38806c64481787d76bc
Ricardo Koller <kollerr@us.ibm.com>
2018-12-10 09:17:50 -0600

    adding run_*_lfs

---
+++ postmark-base/run_all_lfs_cow.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Example:
+# bash run_all_lfs_cow.sh /var/lib/docker/pm_data /var/lib/docker/log.lfs ext4-ord
+
+location=$1
+log=$2
+fs=$3
+
+for workload in read1.small read1.med read1.large read5.small read5.med read5.large read9.small read9.med read9.large
+do
+	for i in `seq 1 5`
+	do
+		#echo 3 > /proc/sys/vm/drop_caches
+		bash run_single_lfs_cow.sh ${workload} ${location} ${log} \
+			2>> data/lfs_cow.${fs}.${workload}.out \
+			>> data/lfs_cow.${fs}.${workload}.tps
+	done
+done
---
+++ postmark-base/run_single_lfs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Example:
+# bash run_single_lfs.sh read5.med pm_data log.lfs
+
+# Location should be empty!!!
+
+workload=$1
+location=$2
+logfile=$3
+
+{
+	echo workload=$1
+	echo location=$2
+	echo logfile=$3
+
+	mkdir -p ${location}
+	rm -rf ${location}/s[0-9]*
+	rm -rf ${location}/[0-9]*
+
+	echo "set location /pm_data" > ${location}/benchmark.${workload}
+	grep -v "set location" benchmark.${workload} >> ${location}/benchmark.${workload}
+
+	genlfs ${location} ${logfile}
+
+	ts=$(date +%s%N)
+	time ./ukvm-bin.seccomp --disk=${logfile} --net=tap100 postmark-split.nabla '{"cmdline":"bin/postmark.nabla /pm_data/benchmark.'${workload}'","net":{"if":"ukvmif0","cloner":"True","type":"inet","method":"static","addr":"10.0.0.2","mask":"16"},"blk":{"source":"etfs","path":"/dev/ld0a","fstype":"blk","mountpoint":"/pm_data"}}'
+	tx=`grep transactions benchmark.${workload} | awk '{print $3}'`
+	tn=$((($(date +%s%N) - $ts)))
+	tt=`echo "scale=8; $tn/1000000000" | bc -l`
+	tp=`echo "scale=4; $tx/$tt" | bc -l`
+	echo "$tx"
+	echo "$tt"
+} 1>&2
+echo "$tp"
