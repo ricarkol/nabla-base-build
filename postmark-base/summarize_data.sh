1d4cc4456a063303657e7cdf4c9266717d43e6cf
Ricardo Koller <kollerr@us.ibm.com>
2018-12-12 08:15:55 -0600

    Adding \n at every line of summarize_data.sh

--- postmark-base/summarize_data.sh
+++ postmark-base/summarize_data.sh
@@ -3,5 +3,5 @@
 (
 	cd data
 	for f in `ls *.tps`; do printf "%-50s " $f;  cat $f \
-		| awk '{printf "%f\n", $1}' | st -nh --m --stddev; done
+		| awk '{printf "%f\n", $1}' | st -nh --m --stddev; printf "\n"; done
 )
