a6212125168089c76f3d1bc18c46ef9d0144448a
Ricardo Koller <kollerr@us.ibm.com>
2019-04-05 22:41:21 -0500

    postmark: run docker should be using volumes

--- postmark-base/run_all_docker.sh
+++ postmark-base/run_all_docker.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Example:
-# bash run_all_docker.sh runc overlay2-ext4-ord
+# bash run_all_docker.sh runc overlay2-ext4-ord /mnt.ext4/pm_data
 
 runtime=$1
 fs=$2
+location=$3
 
 for workload in read1.small read1.med read1.large read5.small read5.med read5.large read9.small read9.med read9.large
 do
 	for i in `seq 1 5`
 	do
 		echo 3 > /proc/sys/vm/drop_caches
-		bash run_single_docker.sh ${workload} ${runtime} \
+		bash run_single_docker.sh ${workload} ${runtime} ${location} \
 			2>> data/${runtime}.${fs}.${workload}.out \
 			>> data/${runtime}.${fs}.${workload}.tps
 	done
--- postmark-base/run_single_docker.sh
+++ postmark-base/run_single_docker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Example:
-# bash run_single_docker.sh read5.med runc
+# bash run_single_docker.sh read5.med runc /mnt.ext4/pm_data
 
 workload=$1
 runtime=$2
+location=$3
+
+echo "set location /vol" > ${location}/benchmark.${workload}
+grep -v location benchmark.${workload} >> ${location}/benchmark.${workload}
 
 docker run -it --runtime=${runtime} --rm --cap-add=NET_ADMIN \
 	--device /dev/net/tun:/dev/net/tun -w /postmark \
-	-v `pwd`:/postmark kollerr/postmark-experiments \
+	-v `pwd`:/postmark \
+	-v ${location}:/vol \
+	kollerr/postmark-experiments \
 	bash -c "bash run_single_linux.sh ${workload} /data 2> /dev/null"
